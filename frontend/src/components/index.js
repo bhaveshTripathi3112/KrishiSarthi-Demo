a3459ff940a0b558aa36ee721c40fc529d11261b
bhavesh <bhaveshtripathi3112@gmail.com>
2025-08-22 22:34:05 +0530

    updated

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,8 @@
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
 
-export { Login , Signup}
+
+export { Login , Signup , Footer , Header , Home}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './layout.jsx'
+import { Home } from './components/index.js'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout/>} >
+      <Route path='' element={<Home/>} />
+    </Route> 
+  )
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router ={router}/>
 )
